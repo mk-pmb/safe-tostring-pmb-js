f15956f4ba193f772d7e104f16156b5a8a6d1106
mk-pmb <mk-pmb@users.noreply.github.com>
2017-09-25 14:36:31 +0200

    v0.1.0: add array examples

--- test.usage.js
+++ test.usage.js
@@ -5,17 +5,28 @@ var eq = require('assert').deepStrictEqual;
 
 (function readmeDemo() {
   //#u
-  var toStr = require('safe-tostring-pmb');
-  eq(toStr(),       'undefined');
+  var toStr = require('safe-tostring-pmb'),
+    obj0 = Object.create(null),
+    noes = new TypeError('Oh noes!'),
+    iter = Symbol.iterator;
+
+    eq(toStr(),       'undefined');
   eq(toStr(null),   'null');
   eq(toStr(23.42),  '23.42');
   eq(toStr('foo'),  'foo');
   eq(toStr(true),   'true');
-  eq(toStr(Object.create(null)),      '[object Object]');
-  eq(toStr(new TypeError('noes')),    'TypeError: noes');
+  eq(toStr({}),     '[object Object]');
+  eq(toStr(obj0),   '[object Object]');
+  eq(toStr(noes),   'TypeError: Oh noes!');
+  eq(toStr([ 1, {}, 2 ]),     '1,[object Object],2');
+  eq(toStr([ 1, obj0, 2 ]),   '[object Array]');
+
   eq(toStr(Symbol('foo')),            'Symbol(foo)');
-  eq(toStr(Symbol.iterator),          'Symbol(Symbol.iterator)');
-  eq(toStr(Object(Symbol.iterator)),  '[object Symbol]');
+  eq(toStr(iter),          'Symbol(Symbol.iterator)');
+  eq(toStr(Object(iter)),  '[object Symbol]');
+  eq(toStr([ 1, Symbol('foo'), 2 ]),      '[object Array]');
+  eq(toStr([ 1, iter, 2 ]),    '[object Array]');
+  eq(toStr([ 1, Object(iter), 2 ]),  '[object Array]');
   //#r
 }());
 
